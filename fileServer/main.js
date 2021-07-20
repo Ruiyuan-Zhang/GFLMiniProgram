fa7770d273c4dff3d7411824a9341c20a5fec756
zhangruiyuan <zhangruiyuan@zju.edu.cn>
2021-07-20 19:20:30 +0800

    feat: 添加了对模型文件的上传管理

--- fileServer/main.js
+++ fileServer/main.js
@@ -5,13 +5,21 @@ import fs from 'fs'
 import stream from 'stream'
 import chalk from 'chalk'
 import boxen from 'boxen'
+import multer from 'multer'
+import stringRandom from 'string-random'
+import path from 'path'
+import os from 'os'
 
 const app = express()
 const port = 3000
 
 
+
 // parse application/x-www-form-urlencoded 未使用
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ 
+    'limit':'100mb',
+    extended: false,
+}))
 
 // parse application/json
 app.use(bodyParser.json({
@@ -38,14 +46,75 @@ function toBuffer(ab) {
 	return buf;
 }
 
+// 允许跨域
+app.use('', (req,res,next)=>{
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next()
+})
+
 // 模型管理静态服务器
-app.use('/file', express.static('model'))
- 
+app.use('/models', express.static('models'))
+
+// 图片文件静态服务器
+app.use('/images', express.static('images'))
+
+
+// let fileName
+
+// let uploadImage = multer({ storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'images')
+//     },
+//     filename: function (req, file, cb) {
+//         fileName = stringRandom(20) + path.extname(file.originalname)
+//         cb(null, fileName)
+//     }
+// })})
+// app.use('/abc' ,uploadImage.single('file'),(req,res,next)=>{
+//     // res.send('shagnchuanchenggong ')
+//     // console.log(req.file, req.body)
+
+//     res.json([{
+//         ok: 200,
+//         url: `/images/${fileName}`
+//     }])
+// })
+
+// 文件上传服务器（后台）
+const uploadServer = (url, dir) =>{
+    let fileName
+    let uploadImage = multer({ storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, dir)
+        },
+        filename: function (req, file, cb) {
+            fileName = stringRandom(20) + path.extname(file.originalname)
+            cb(null, fileName)
+        }
+    })})
+    app.use(url ,uploadImage.single('file'),(req,res,next)=>{
+        // res.send('shagnchuanchenggong ')
+        // console.log(req.file, req.body)
+    
+        res.json([{
+            ok: 200,
+            url: `/${dir}/${fileName}`
+        }])
+    })
+}
+
+// 实例化文件上传路由
+uploadServer('/uploadImage','images/back')
+uploadServer('/uploadGlobalModel','models/globalModel')
+uploadServer('/uploadClientModel','models/clentModel')
+
+// 全局模型上传服务器
 app.use('/upload',(req,res)=>{
     // 0 简单的输出一下
-    let data = req.body.mp
-
-    console.log(data)
+    // let data = req.body.mp
+    // console.log(data)
 
     // 1 处理base64数据
     for (let key of Object.keys(data)){
@@ -80,8 +149,7 @@ app.use('/upload',(req,res)=>{
 })
 
 
-import os from 'os'
-///获取本机ip///
+///获取本机ip
 function getIPAdress() {
     var interfaces = os.networkInterfaces();
     for (var devName in interfaces) {
@@ -96,15 +164,27 @@ function getIPAdress() {
 }
 const myHost = getIPAdress();
 
-
 app.listen(port)
 
+// 打印情况
 let message = chalk.green('Serving!');
 message += `\n\n${chalk.bold(`文件模型访问请使用:`)} `;
-message += `\n${chalk.bold(`- 本地:`)}         http://localhost/file`;
-message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/file`;
-message += `\n\n${chalk.bold(`上传模型文件请使用:`)} `;
-message += `\n${chalk.bold(`- 本地:`)}         http://localhost/upload`;
+message += `\n${chalk.bold(`- 本地:`)}         http://localhost:${port}/models`;
+message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/models`;
+message += `\n\n${chalk.bold(`图片访问请使用:`)} `;
+message += `\n${chalk.bold(`- 本地:`)}         http://localhost:${port}/images`;
+message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/images`;
+message += `\n\n${chalk.bold(`上传图片文件请使用:`)} `;
+message += `\n${chalk.bold(`- 本地:`)}         http://localhost:${port}/uploadImage`;
+message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/uploadImage`;
+message += `\n\n${chalk.bold(`上传全局模型文件请使用:`)} `;
+message += `\n${chalk.bold(`- 本地:`)}         http://localhost:${port}/uploadGlobalModel`;
+message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/uploadGlobalModel`;
+message += `\n\n${chalk.bold(`上传客户端模型文件请使用:`)} `;
+message += `\n${chalk.bold(`- 本地:`)}         http://localhost:${port}/uploadClientModel`;
+message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/uploadClientModel`;
+message += `\n\n${chalk.bold(`上传模型文件请使用「tfjs」:`)} `;
+message += `\n${chalk.bold(`- 本地:`)}         http://localhost:${port}/upload`;
 message += `\n${chalk.bold('- 局域网:')}       http://${myHost}:${port}/upload`;
 console.log(boxen(message, {
     padding: 1,
