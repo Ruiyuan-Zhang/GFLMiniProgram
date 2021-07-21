114468acbae66d8de8c974101255251a1b612891
zhangruiyuan <zhangruiyuan@zju.edu.cn>
2021-07-22 00:36:04 +0800

    feat: 登录界面

---
+++ client/src/pages/login/index.js
@@ -0,0 +1,38 @@
+import { View, Text, Input } from '@tarojs/components'
+import styles from './index.module.less'
+import {saveToken, getToken, removeToken} from '@/common/token'
+import Taro, { pageScrollTo } from '@tarojs/taro'
+import { useEffect, useState } from 'react'
+import { AtAvatar, AtForm, AtInput, AtButton } from 'taro-ui'
+import fetch from '@/utils/request'
+
+const index = () =>{
+    const [imgUrl,setImgUrl] = useState()
+    const [name,setName] = useState()
+    const [pass,setPass] = useState()
+    const login = async ()=>{
+        if(name&&pass){
+            let {token} = await fetch({url: '/v1/admin/users/login',data: {user_name:name,pass}})
+            saveToken(token)
+            Taro.navigateTo({url:'/pages/home/index'})
+        }
+    }
+    useEffect(async()=>{
+        // 必须是在用户已经授权的情况下调用
+        let user = await Taro.getUserInfo()
+        console.log(user.userInfo.avatarUrl)
+        setImgUrl(user.userInfo.avatarUrl)
+    },[])
+    return (
+        <View className={styles.index}>
+            <AtAvatar image={imgUrl} size='large' circle></AtAvatar>
+            <View className={styles.form}>
+                <AtInput title='用户名' type='text' placeholder='请输入您的用户名' name='name' value={name} onChange={setName}/>
+                <AtInput title='密码' type='password' placeholder='请输入您的密码' name='pass' value={pass} onChange={setPass}/>
+                <AtButton type='primary' onClick={login}>登录</AtButton>
+            </View>
+        </View>
+    )
+}
+
+export default index
